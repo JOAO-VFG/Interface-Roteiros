8a32b9e3f39729c909bd1d489a547140df542a3e
JOAO-VFG <78110240+JOAO-VFG@users.noreply.github.com>
2021-02-04 13:38:58 -0300

    att

--- Interface-Roteiros/Roteiro-2.Designer.cs
+++ Interface-Roteiros/Roteiro-2.Designer.cs
@@ -29,6 +29,7 @@ namespace Interface_Roteiros
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             this.panel1 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
@@ -44,10 +45,19 @@ namespace Interface_Roteiros
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+=======
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Roteiro_2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+>>>>>>> Stashed changes
             this.SuspendLayout();
             // 
             // panel1
             // 
+<<<<<<< Updated upstream
             this.panel1.BackColor = System.Drawing.Color.Blue;
             this.panel1.Location = new System.Drawing.Point(0, 333);
             this.panel1.Name = "panel1";
@@ -88,10 +98,33 @@ namespace Interface_Roteiros
             this.button1.Size = new System.Drawing.Size(194, 49);
             this.button1.TabIndex = 3;
             this.button1.Text = "INICIAR";
+=======
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(125)))), ((int)(((byte)(115)))));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 402);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(143)))), ((int)(((byte)(145)))));
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(480, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PDF";
+>>>>>>> Stashed changes
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+<<<<<<< Updated upstream
             this.button2.BackColor = System.Drawing.Color.Aqua;
             this.button2.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(22, 80);
@@ -146,11 +179,31 @@ namespace Interface_Roteiros
             this.label2.Size = new System.Drawing.Size(466, 100);
             this.label2.TabIndex = 1;
             this.label2.Text = " ";
+=======
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(52)))), ((int)(((byte)(235)))));
+            this.button2.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(480, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "INICIAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(145)))), ((int)(((byte)(53)))));
+            this.panel2.Location = new System.Drawing.Point(114, 149);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(359, 118);
+            this.panel2.TabIndex = 2;
+>>>>>>> Stashed changes
             // 
             // Roteiro_2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< Updated upstream
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
@@ -165,6 +218,12 @@ namespace Interface_Roteiros
             this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+=======
+            this.Controls.Add(this.panel1);
+            this.Name = "Roteiro_2";
+            this.Size = new System.Drawing.Size(580, 402);
+            this.panel1.ResumeLayout(false);
+>>>>>>> Stashed changes
             this.ResumeLayout(false);
 
         }
@@ -172,6 +231,7 @@ namespace Interface_Roteiros
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+<<<<<<< Updated upstream
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Panel panel2;
@@ -182,5 +242,10 @@ namespace Interface_Roteiros
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+=======
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+>>>>>>> Stashed changes
     }
 }
