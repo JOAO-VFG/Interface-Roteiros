1beaf1b542c36b125f34f26fa5504da102dee43e
JOAO-VFG <78110240+JOAO-VFG@users.noreply.github.com>
2021-02-04 13:37:28 -0300

    att

--- Interface-Roteiros/Roteiro-2.Designer.cs
+++ Interface-Roteiros/Roteiro-2.Designer.cs
@@ -38,6 +38,9 @@ namespace Interface_Roteiros
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -62,6 +65,7 @@ namespace Interface_Roteiros
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.BackColor = System.Drawing.Color.Black;
+            this.flowLayoutPanel2.Controls.Add(this.label1);
             this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 36);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(583, 31);
@@ -77,35 +81,38 @@ namespace Interface_Roteiros
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.SlateBlue;
             this.button1.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(59, 12);
+            this.button1.Location = new System.Drawing.Point(22, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(194, 49);
             this.button1.TabIndex = 3;
             this.button1.Text = "INICIAR";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
             this.button2.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(59, 60);
+            this.button2.Location = new System.Drawing.Point(22, 80);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(194, 49);
             this.button2.TabIndex = 4;
             this.button2.Text = "PDF";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.button2);
             this.panel3.Controls.Add(this.button1);
-            this.panel3.Location = new System.Drawing.Point(195, 217);
+            this.panel3.Location = new System.Drawing.Point(195, 195);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.Size = new System.Drawing.Size(234, 132);
             this.panel3.TabIndex = 5;
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.label2);
             this.panel4.Controls.Add(this.pictureBox1);
             this.panel4.Location = new System.Drawing.Point(3, 89);
             this.panel4.Name = "panel4";
@@ -114,11 +121,31 @@ namespace Interface_Roteiros
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox1.Size = new System.Drawing.Size(105, 100);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LEIS DE OHM E POTÊNCIAS";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(466, 100);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
             // 
             // Roteiro_2
             // 
@@ -133,6 +160,8 @@ namespace Interface_Roteiros
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Roteiro_2";
             this.Size = new System.Drawing.Size(580, 402);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -151,5 +180,7 @@ namespace Interface_Roteiros
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
