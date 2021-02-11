12246d22e8cbfad94c665cf73bcf7fc53f0c8c1d
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-02-11 12:29:26 -0400

    Updates

---
+++ Interface-Roteiros/TelaAjustes.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Interface_Roteiros
+{
+    partial class TelaAjustes
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xuiCircleProgressBar1 = new XanderUI.XUICircleProgressBar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(54)))), ((int)(((byte)(74)))));
+            this.panel1.Controls.Add(this.xuiCircleProgressBar1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(83, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 192);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resistor";
+            // 
+            // xuiCircleProgressBar1
+            // 
+            this.xuiCircleProgressBar1.AnimationSpeed = 5;
+            this.xuiCircleProgressBar1.FilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(220)))), ((int)(((byte)(210)))));
+            this.xuiCircleProgressBar1.FilledColorAlpha = 130;
+            this.xuiCircleProgressBar1.FilledThickness = 14;
+            this.xuiCircleProgressBar1.IsAnimated = false;
+            this.xuiCircleProgressBar1.Location = new System.Drawing.Point(14, 29);
+            this.xuiCircleProgressBar1.Name = "xuiCircleProgressBar1";
+            this.xuiCircleProgressBar1.Percentage = 63;
+            this.xuiCircleProgressBar1.ShowText = true;
+            this.xuiCircleProgressBar1.Size = new System.Drawing.Size(87, 88);
+            this.xuiCircleProgressBar1.TabIndex = 1;
+            this.xuiCircleProgressBar1.TextColor = System.Drawing.Color.Gray;
+            this.xuiCircleProgressBar1.TextSize = 12;
+            this.xuiCircleProgressBar1.UnFilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
+            this.xuiCircleProgressBar1.UnfilledThickness = 4;
+            // 
+            // TelaAjustes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "TelaAjustes";
+            this.Size = new System.Drawing.Size(599, 334);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private XanderUI.XUICircleProgressBar xuiCircleProgressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
