cbafc4dbde838c6f768bc7f8bdd704cad2c5acc1
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-02-11 17:33:01 -0400

    Tela de Suporte
    Adicionado slider e outros componentes a tela de suporte

--- Interface-Roteiros/TelaAjustes.Designer.cs
+++ Interface-Roteiros/TelaAjustes.Designer.cs
@@ -29,29 +29,42 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.xuiCircleProgressBar1 = new XanderUI.XUICircleProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sldResistencia = new XanderUI.XUISlider();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(54)))), ((int)(((byte)(74)))));
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.sldResistencia);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.xuiCircleProgressBar1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnExit);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(83, 42);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(306, 192);
+            this.panel1.Size = new System.Drawing.Size(410, 240);
             this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Resistor";
             // 
             // xuiCircleProgressBar1
             // 
@@ -60,26 +73,159 @@
             this.xuiCircleProgressBar1.FilledColorAlpha = 130;
             this.xuiCircleProgressBar1.FilledThickness = 14;
             this.xuiCircleProgressBar1.IsAnimated = false;
-            this.xuiCircleProgressBar1.Location = new System.Drawing.Point(14, 29);
+            this.xuiCircleProgressBar1.Location = new System.Drawing.Point(35, 79);
             this.xuiCircleProgressBar1.Name = "xuiCircleProgressBar1";
-            this.xuiCircleProgressBar1.Percentage = 63;
+            this.xuiCircleProgressBar1.Percentage = 20;
             this.xuiCircleProgressBar1.ShowText = true;
-            this.xuiCircleProgressBar1.Size = new System.Drawing.Size(87, 88);
+            this.xuiCircleProgressBar1.Size = new System.Drawing.Size(110, 110);
             this.xuiCircleProgressBar1.TabIndex = 1;
             this.xuiCircleProgressBar1.TextColor = System.Drawing.Color.Gray;
-            this.xuiCircleProgressBar1.TextSize = 12;
+            this.xuiCircleProgressBar1.TextSize = 15;
             this.xuiCircleProgressBar1.UnFilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(114)))), ((int)(((byte)(114)))));
             this.xuiCircleProgressBar1.UnfilledThickness = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(2, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resistor";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImage = global::Interface_Roteiros.Properties.Resources.cancel;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(380, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(24, 24);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(410, 4);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resistência (Ω):";
+            // 
+            // sldResistencia
+            // 
+            this.sldResistencia.BarThickness = 4;
+            this.sldResistencia.BigStepIncrement = 10;
+            this.sldResistencia.FilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(119)))), ((int)(((byte)(215)))));
+            this.sldResistencia.KnobColor = System.Drawing.Color.Gray;
+            this.sldResistencia.KnobImage = null;
+            this.sldResistencia.Location = new System.Drawing.Point(0, 207);
+            this.sldResistencia.Name = "sldResistencia";
+            this.sldResistencia.Percentage = 20;
+            this.sldResistencia.QuickHopping = false;
+            this.sldResistencia.Size = new System.Drawing.Size(167, 20);
+            this.sldResistencia.SliderStyle = XanderUI.XUISlider.Style.Windows10;
+            this.sldResistencia.TabIndex = 5;
+            this.sldResistencia.UnfilledColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(169)))), ((int)(((byte)(219)))));
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(19)))), ((int)(((byte)(38)))));
+            this.panel3.Location = new System.Drawing.Point(163, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(7, 207);
+            this.panel3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(67, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Informações";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(199)))), ((int)(((byte)(201)))));
+            this.label4.Location = new System.Drawing.Point(176, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Corrente:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(199)))), ((int)(((byte)(201)))));
+            this.label5.Location = new System.Drawing.Point(179, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Potência:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(199)))), ((int)(((byte)(201)))));
+            this.label6.Location = new System.Drawing.Point(260, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(199)))), ((int)(((byte)(201)))));
+            this.label7.Location = new System.Drawing.Point(260, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0W";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(172)))), ((int)(((byte)(227)))));
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(170, 33);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(240, 35);
+            this.panel4.TabIndex = 12;
             // 
             // TelaAjustes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Controls.Add(this.panel1);
             this.Name = "TelaAjustes";
-            this.Size = new System.Drawing.Size(599, 334);
+            this.Size = new System.Drawing.Size(410, 240);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -89,5 +235,16 @@
         private System.Windows.Forms.Panel panel1;
         private XanderUI.XUICircleProgressBar xuiCircleProgressBar1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnExit;
+        private XanderUI.XUISlider sldResistencia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
     }
 }
