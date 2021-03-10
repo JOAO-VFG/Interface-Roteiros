c133355523a1e64ea074d64dc2ffea8aeb37950c
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-03-10 11:10:25 -0400

    Menu navegação excluído

--- Interface-Roteiros/CaixaComponente.cs
+++ Interface-Roteiros/CaixaComponente.cs
@@ -27,21 +27,37 @@ namespace Interface_Roteiros
             get { return imgImagem.Image; }
             set { imgImagem.Image = value; }
         }
+        public override Color ForeColor
+        {
+            get { return lblNomeCompoenente.ForeColor; }
+            set { lblNomeCompoenente.ForeColor = value; }
+        }
+
+        public override Color BackColor
+        {
+            get { return lblNomeCompoenente.BackColor; }
+            set
+            {
+                lblNomeCompoenente.BackColor = value;
+                imgImagem.BackColor = value;
+            }
+        }
+
+        // Cor do componente quando o mouse sai do campo
+        private Color BackColorLeave;
 
         public bool IsOpen { get; private set; }
-        private readonly Color corVerde = Color.FromArgb(21, 232, 46);
         private readonly Color corPadrao = Color.Transparent;
         // Campos privados para evento Enter
         private readonly Color corRosa = Color.FromArgb(193, 27, 222);
         private readonly Color corBranca = Color.White;
-        // Campo privado para evento Leave
-        private readonly Color corEscura = Color.FromArgb(18, 18, 18);
 
         public CaixaComponente()
         {
             InitializeComponent();
         }
 
+        // O item é clicado, abre e fecha ele
         private void lblNomeCompoenente_Click(object sender, EventArgs e)
         {
             if (IsOpen)
@@ -69,20 +85,21 @@ namespace Interface_Roteiros
         {
             if(IsOpen)
             {
-                panSelecionado.BackColor = corVerde;
+                panSelecionado.BackColor = CorOnSelected;
             }
             else
             {
                 panSelecionado.BackColor = corPadrao;
             }
-            lblNomeCompoenente.BackColor = corEscura;
-            imgImagem.BackColor = corEscura;
+
+            lblNomeCompoenente.BackColor = BackColorLeave;
+            imgImagem.BackColor = BackColorLeave;
         }
 
         private void FecharJanela()
         {
             this.Size = this.MinimumSize;
-            panSelecionado.BackColor = corPadrao;
+            panSelecionado.BackColor = BackColorLeave;
             lblNomeCompoenente.Padding = new Padding(30, 0, 0, 0);
             imgImagem.Location = new Point(6, 12);
         }
@@ -90,7 +107,7 @@ namespace Interface_Roteiros
         private void AbrirJanela()
         {
             this.Size = this.MaximumSize;
-            panSelecionado.BackColor = corVerde;
+            panSelecionado.BackColor = CorOnSelected;
             lblNomeCompoenente.Padding = new Padding(35, 0, 0, 0);
             imgImagem.Location = new Point(10, 12);
         }
@@ -98,6 +115,7 @@ namespace Interface_Roteiros
         private void CaixaComponente_Load(object sender, EventArgs e)
         {
             IsOpen = false;
+            BackColorLeave = BackColor;
         }
 
         private void tckResistencia_Scroll(object sender, EventArgs e)
