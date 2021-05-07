c9381731e54b0305822706333607aac339f85263
Gabriel Bento <Gabriel_holanda@hotmail.com.br>
2021-05-07 10:42:12 -0400

    Tela de Cores do Resistor Feita

---
+++ Interface-Roteiros/TelaCoresResistor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interface_Roteiros
+{
+    public partial class TelaCoresResistor : Interface_Roteiros.TelaInicialPadrao
+    {
+        public TelaCoresResistor()
+        {
+            InitializeComponent();
+        }
+    }
+}
